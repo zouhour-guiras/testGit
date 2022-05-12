99ca28d12945c771be11bc864fabfed4cee44af1
zouhour-guiras <guiras.zouhour@gmail.com>
2022-05-12 12:09:25 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/testGit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testGit.dir/main.c.obj"
+  "libtestGit.dll.a"
+  "testGit.exe"
+  "testGit.exe.manifest"
+  "testGit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testGit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FIZZ "Fizz"
+#define BUZZ "Buzz"
+int main (void) {
+    for (int i = 1; i <= 100; i++) {
+        if (i % 15 == 0) {
+            printf ("%s%s\n", FIZZ, BUZZ);
+        } else if (i % 3 == 0) {
+            printf ("%s\n", FIZZ);
+        } else if (i % 5 == 0) {
+            printf ("%s\n", BUZZ);
+        } else {
+            printf ("%d\n", i);
+        }
+    }
+    return EXIT_SUCCESS;
+
+        }
